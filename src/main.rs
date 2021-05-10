98762f521601ba2e208368b0b871a2f1186052e8
Tyrving <70444887+Tyrving@users.noreply.github.com>
2021-05-10 18:54:01 -0400

    saving to other laptop

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,99 @@
-use std::io;
+use std::io::stdin;
+use std::env::args;
+const TAPE_LEN:usize = 256;
+
+fn print_tape(tape :[u8; TAPE_LEN], pointer :usize) -> [u8; TAPE_LEN] {
+    let mut last_nonzero :usize;
+
+    for i in 0..TAPE_LEN-1 {
+        if tape[i] != 0 {
+            last_nonzero = index;
+        }
+    }
+
+    for byte in tape.iter(){
+        print!("{}|", byte);
+    }
+
+    println!();
+
+}
 
 fn main() {
     println!("Welcome to Mark's BF compiler, written in rust!");
+    println!("Input BF program below. I do not support nested loops.");
+
     let mut pointer: usize = 0; //use for tape index
-    let mut in_char_num: usize = 0; //current character to check
-    let mut loop_depth: usize = 0; //for loops within loops
-    let mut tape: [u8; 25] = [0; 25]; //init tape to 0's
+    let mut input_index: usize = 0; //current character to check
+    let mut tape: [u8; TAPE_LEN] = [0; TAPE_LEN]; //init tape to 0's
     let mut input: String = String::new(); //input characters
     let mut skip_forward: bool = false; //skip over loop
     let mut skip_back: bool = false;
     let mut return_string: String = String::new(); //add output characters on '.' to this variable
 
-    io::stdin() //  read line of input characters
+    stdin() //  read line of input characters
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    //use this to go over each character,
-    // incrementing and decrementing in_char_num for loops and such
-    while in_char_num < input.len()-1 {
+
+    while input_index < input.len()-1 {
 
         //assign current character to variable
-        let char_char: char = input.as_bytes()[in_char_num] as char;
+        let current_char: char = input.as_bytes()[input_index] as char;
 
         if skip_back {
-            if char_char == '[' {
+            if current_char == '[' {
                 skip_back = false;
                 continue;
             }
-            in_char_num -= 1;
+            input_index -= 1;
             continue;
         } else if skip_forward {
-            if char_char == ']' {
+            if current_char == ']' {
                 skip_forward = false;
                 continue;
             }
-            in_char_num += 1;
+            input_index += 1;
             continue;
         }
 
         //print out current tape
-        for byte in tape.iter() {
-            print!("{}|", byte);
-        }
-        println!();
+        print_tape(tape, pointer);
 
-        match char_char {
+        match current_char {
             '+' => {
                 tape[pointer] += 1;
-                in_char_num += 1;
+                input_index += 1;
             }
             '-' => {
                 tape[pointer] -= 1;
-                in_char_num += 1;
+                input_index += 1;
             }
             '>' => {
                 pointer += 1;
-                in_char_num += 1;
+                input_index += 1;
             }
             '<' => {
                 pointer -= 1;
-                in_char_num += 1;
+                input_index += 1;
             }
             ',' => {
 
             }
             '.' => {
                 return_string += &(tape[pointer] as char).to_string();
-                in_char_num += 1;
+                input_index += 1;
             }
             '[' => {
                 if tape[pointer] == 0 {
                     skip_forward = true;
                 } else {
-                    in_char_num += 1;
-                    loop_depth += 1;
+                    input_index += 1;
                 }
             }
             ']' => {
                 if tape[pointer] == 0 {
-                    in_char_num += 1;
-                    loop_depth -= 1;
+                    input_index += 1;
                 } else {
                     skip_back = true;
                 }
@@ -89,8 +103,8 @@ fn main() {
         }
         println!(
             "Current Char: {} ({}) of {}. Value {}",
-            in_char_num,
-            char_char,
+            input_index,
+            current_char,
             input.len(),
             tape[pointer]
         );
